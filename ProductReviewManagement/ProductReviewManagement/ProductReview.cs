af809b68e624f4f07d368f5209d95aa749b93f50
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-20 23:47:57 +0530

    [Sanju] ADD : Taking details of product

---
+++ ProductReviewManagement/ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    class ProductReview
+    {
+        public int ProductId
+        {
+            get;
+            set;
+        }
+
+        public int UserId
+        {
+            get;
+            set;
+        }
+        public double Rating
+        {
+            get;
+            set;
+        }
+        public string Review
+        {
+            get;
+            set;
+        }
+        public bool isLike
+        {
+            get;
+            set;
+        }
+    }
+}
+
---
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("======================== WELCOME TO PRODUCT REVIEW MANAGEMENT =================================");
+
+                List<ProductReview> list = new List<ProductReview>()
+            {
+            new ProductReview() { ProductId = 1, UserId = 1, Rating = 6.5, Review = "Good", isLike=true},
+            new ProductReview() { ProductId = 1, UserId = 1, Rating = 4, Review = "Bad", isLike=true },
+            new ProductReview() { ProductId = 2, UserId = 1, Rating = 6, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 2, UserId = 1, Rating = 5, Review = "Average", isLike=true },
+            new ProductReview() { ProductId = 4, UserId = 1, Rating = 6, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 5, UserId = 1, Rating = 9, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 6, UserId = 1, Rating = 9.5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 7, UserId = 1, Rating = 7.5, Review = "Good", isLike=false},
+            new ProductReview() { ProductId = 4, UserId = 1, Rating = 8.5, Review = "Good", isLike=false },
+            new ProductReview() { ProductId = 9, UserId = 1, Rating = 6.5, Review = "Good", isLike=false },
+            new ProductReview() { ProductId = 10, UserId = 1, Rating = 5, Review = "Average", isLike=false },
+            new ProductReview() { ProductId = 11, UserId = 1, Rating = 3, Review = "Bad", isLike=true },
+            new ProductReview() { ProductId = 12, UserId = 1, Rating = 5, Review = "Average", isLike=true },
+            new ProductReview() { ProductId = 13, UserId = 1, Rating = 3.5, Review = "Bad", isLike=true },
+            new ProductReview() { ProductId = 14, UserId = 1, Rating = 5, Review = "Average", isLike=true },
+            new ProductReview() { ProductId = 15, UserId = 1, Rating = 4, Review = "Bad", isLike=true }
+            };
+                Console.WriteLine("Table rows are :  ");
+                foreach (var listData in list)
+                {
+                    Console.WriteLine("Product id is = " + listData.ProductId + "User id is = " + listData.UserId + "Rating is = " + listData.Rating + " Review is = " + listData.Review + " isLike = " + listData.isLike);
+                }
+            }
+        }
+}
+
