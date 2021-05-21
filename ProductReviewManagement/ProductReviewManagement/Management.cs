4e011a8c8bf07b0829c1d7e8e879f10518497638
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-05-21 19:31:07 +0530

    [Sanju] ADD : Create data table using linq and add data into table.

--- ProductReviewManagement/ProductReviewManagement/Management.cs
+++ ProductReviewManagement/ProductReviewManagement/Management.cs
@@ -80,5 +80,46 @@ namespace ProductReviewManagement
             Console.WriteLine("\nAfter skipping top 5 records from list : ");
             Display(skip5Records);
         }
+
+        /// <summary>
+        /// Creating data table.
+        /// </summary>
+        public void CreateDataTable()
+        {
+            //Declaring Data table column
+            DataTable tableData = new DataTable();
+            tableData.Columns.Add("ProducID");
+            tableData.Columns.Add("UserID");
+            tableData.Columns.Add("Rating");
+            tableData.Columns.Add("Review");
+            tableData.Columns.Add("isLike");
+            //Adding data in rows
+            tableData.Rows.Add(5, 7, 4, "Bad", false);
+            tableData.Rows.Add(3, 6, 15, "Good", true);
+            tableData.Rows.Add(2, 5, 18, "Good", true);
+            tableData.Rows.Add(4, 4, 17, "Good", true);
+            tableData.Rows.Add(8, 3, 6, "Bad", false);
+            tableData.Rows.Add(7, 2, 10, "Average", true);
+            tableData.Rows.Add(11, 1, 10, "Average", true);
+            tableData.Rows.Add(2, 12, 8, "Bad", false);
+            tableData.Rows.Add(6, 10, 10, "Average", true);
+            tableData.Rows.Add(4, 9, 16, "Good", true);
+            tableData.Rows.Add(6, 8, 10, "Average", true);
+            //Display data table of all records
+            DisplayDataTable(tableData);
+        }
+
+        public void DisplayDataTable(DataTable table)
+        {
+            var dataStore = (from products in table.AsEnumerable()
+                           select products);
+
+            Console.WriteLine("\nAll Data Table Records are : ");
+            foreach (DataRow row in dataStore)
+            {
+                Console.WriteLine("ProducID = {0}, UserID = {1}, Rating = {2}, Review = {3}, isLike = {4}", row["ProducID"], row["UserID"], row["Rating"], row["Review"], row["isLike"]);
+            }
+        }
+
     }
 }
--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace ProductReviewManagement
 {
@@ -12,21 +13,21 @@ namespace ProductReviewManagement
                 List<ProductReview> list = new List<ProductReview>()
             {
             new ProductReview() { ProductId = 1, UserId = 1, Rating = 6.5, Review = "Good", isLike=true},
-            new ProductReview() { ProductId = 1, UserId = 1, Rating = 4, Review = "Bad", isLike=true },
-            new ProductReview() { ProductId = 2, UserId = 1, Rating = 6, Review = "Good", isLike=true },
-            new ProductReview() { ProductId = 2, UserId = 1, Rating = 5, Review = "Average", isLike=true },
-            new ProductReview() { ProductId = 4, UserId = 1, Rating = 6, Review = "Good", isLike=true },
-            new ProductReview() { ProductId = 5, UserId = 1, Rating = 9, Review = "Good", isLike=true },
-            new ProductReview() { ProductId = 6, UserId = 1, Rating = 9.5, Review = "Good", isLike=true },
-            new ProductReview() { ProductId = 7, UserId = 1, Rating = 7.5, Review = "Good", isLike=false},
-            new ProductReview() { ProductId = 4, UserId = 1, Rating = 8.5, Review = "Good", isLike=false },
-            new ProductReview() { ProductId = 9, UserId = 1, Rating = 6.5, Review = "Good", isLike=false },
-            new ProductReview() { ProductId = 10, UserId = 1, Rating = 5, Review = "Average", isLike=false },
-            new ProductReview() { ProductId = 11, UserId = 1, Rating = 3, Review = "Bad", isLike=true },
-            new ProductReview() { ProductId = 12, UserId = 1, Rating = 5, Review = "Average", isLike=true },
-            new ProductReview() { ProductId = 13, UserId = 1, Rating = 3.5, Review = "Bad", isLike=true },
-            new ProductReview() { ProductId = 14, UserId = 1, Rating = 5, Review = "Average", isLike=true },
-            new ProductReview() { ProductId = 15, UserId = 1, Rating = 4, Review = "Bad", isLike=true }
+            new ProductReview() { ProductId = 1, UserId = 2, Rating = 4, Review = "Bad", isLike=true },
+            new ProductReview() { ProductId = 2, UserId = 3, Rating = 6, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 2, UserId = 4, Rating = 5, Review = "Average", isLike=true },
+            new ProductReview() { ProductId = 4, UserId = 5, Rating = 6, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 5, UserId = 6, Rating = 9, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 6, UserId = 7, Rating = 9.5, Review = "Good", isLike=true },
+            new ProductReview() { ProductId = 7, UserId = 8, Rating = 7.5, Review = "Good", isLike=false},
+            new ProductReview() { ProductId = 4, UserId = 9, Rating = 8.5, Review = "Good", isLike=false },
+            new ProductReview() { ProductId = 9, UserId = 10, Rating = 6.5, Review = "Good", isLike=false },
+            new ProductReview() { ProductId = 10, UserId = 11, Rating = 5, Review = "Average", isLike=false },
+            new ProductReview() { ProductId = 11, UserId = 12, Rating = 3, Review = "Bad", isLike=true },
+            new ProductReview() { ProductId = 12, UserId = 13, Rating = 5, Review = "Average", isLike=true },
+            new ProductReview() { ProductId = 13, UserId = 14, Rating = 3.5, Review = "Bad", isLike=true },
+            new ProductReview() { ProductId = 14, UserId = 15, Rating = 5, Review = "Average", isLike=true },
+            new ProductReview() { ProductId = 15, UserId = 16, Rating = 4, Review = "Bad", isLike=true }
             };
 
                 Console.WriteLine("Table rows are :  ");
@@ -43,7 +44,10 @@ namespace ProductReviewManagement
 
             //Will skip top 5 records from list and display rest data.
             management.SkipTopFiveRecords(list);
-            }
-       }
-}
 
+            //Data table
+            management.CreateDataTable();
+
+        }
+    }
+}
